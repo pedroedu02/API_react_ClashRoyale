6fccd952c88282dd872c59c8440ac1625381a297
Pedro Garcia <pedroedu.gc02@gmail.com>
2024-04-20 16:33:06 -0300

    Ajustado CSS da grid dos filmes + metodo para pesquisar filmes

--- API_Movies/src/Components/Navbar.jsx
+++ API_Movies/src/Components/Navbar.jsx
@@ -1,26 +1,42 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiSearchAlt2 } from "react-icons/bi"
 
 import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav id='navbar'>
-            <h2>
-              <Link to="/"> 
-                Filmes
-              </Link> 
-            </h2>
-            <form>
-                <input type="text" placeholder="Busque um filme" />
-                <button type="submit"> 
-                    <BiSearchAlt2 />
-                </button>
-            </form>
-             
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
-       </nav>
-    )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          Filmes
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Pesquise um filme aqui"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
 
 export default Navbar;
